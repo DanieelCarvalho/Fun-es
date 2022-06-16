de64671981e95552930cb1aae8c217e9f2e85414
DanieelCarvalho <Danielscg2012@gmail.com>
2022-06-16 15:35:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function time(){
+    console.log('Vasco da Gama')
+}
+time()
+
+
+function mostrarNome(nome){
+    console.log(`Meu nome é ${nome}`)
+}
+mostrarNome("Daniel")
+
+function resposta( nome, idade, musica){
+    console.log("Meu nome é " + nome + " eu tenho " + idade + " anos " + "e meu estilo musical preferido é " + musica)
+}
+resposta("Daniel",29,"Samba")
+
+function curiosidade (filme, musica){
+    console.log("A resposta é :" + filme +  ", " + musica)
+}
+curiosidade("Matrix 1", "Foi um Rio que Passou em Minha Vida")
+
+function bonus(numero){
+    console.log(3 * numero)
+}
+bonus(2)
